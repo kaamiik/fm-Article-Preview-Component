6b4bccf36354730160ba92d65da80b0bdfd6be64
Kamran Kiani <kiani.km@gmail.com>
2024-03-21 14:52:06 +0330

    Solve some bugs

--- script.js
+++ script.js
@@ -6,45 +6,64 @@ const cardProfileEl = document.querySelector(".card__profile");
 const cardShareEl = document.querySelector(".card__share--mobile");
 const cardShareDesktopEl = document.querySelector(".card__share--desktop");
 
-let isShareOpen = false;
-let wasButtonClickedOnMobile = false;
-let wasButtonClickedOnDesktop = false;
+// Initialize aria-expanded attribute
+btnProfile.setAttribute("aria-expanded", "false");
+btnShare.setAttribute("aria-expanded", "false");
+
+// let isShareOpen = false;
+// let wasButtonClickedOnMobile = false;
+// let wasButtonClickedOnDesktop = false;
 
 const defaultState = () => {
   cardProfileEl.classList.remove("visually-hidden");
   cardShareEl.classList.add("visually-hidden");
   cardShareDesktopEl.classList.add("visually-hidden");
+  btnProfile.setAttribute("aria-expanded", "false");
+  btnShare.setAttribute("aria-expanded", "false");
 };
 
 btnProfile.addEventListener("click", function () {
-  if (window.innerWidth <= 780) {
+  if (window.innerWidth <= 869) {
     cardProfileEl.classList.add("visually-hidden");
     cardShareEl.classList.remove("visually-hidden");
-    isShareOpen = true;
-    wasButtonClickedOnMobile = true;
+    btnProfile.setAttribute("aria-expanded", "true");
+    // isShareOpen = true;
+    // wasButtonClickedOnMobile = true;
   } else {
     cardShareDesktopEl.classList.toggle("visually-hidden");
-    wasButtonClickedOnDesktop = true;
+    let isExpaned = btnProfile.getAttribute("aria-expanded") === "true";
+    btnProfile.setAttribute("aria-expanded", !isExpaned);
+    // wasButtonClickedOnDesktop = true;
   }
 });
 
 btnShare.addEventListener("click", function () {
-  if (window.innerWidth <= 780 && isShareOpen) {
+  if (
+    window.innerWidth <= 869 &&
+    btnProfile.getAttribute("aria-expanded") === "true"
+  ) {
     cardProfileEl.classList.remove("visually-hidden");
     cardShareEl.classList.add("visually-hidden");
-    isShareOpen = false;
-    wasButtonClickedOnMobile = false;
+    btnProfile.setAttribute("aria-expanded", "false");
+    // isShareOpen = false;
+    // wasButtonClickedOnMobile = false;
   }
 });
 
 window.addEventListener("resize", function () {
-  if (window.innerWidth <= 780 && wasButtonClickedOnDesktop) {
+  if (
+    window.innerWidth <= 860 &&
+    btnProfile.getAttribute("aria-expanded") === "true"
+  ) {
     // Reset to default state for mobile view
     defaultState();
-    wasButtonClickedOnDesktop = false;
-  } else if (window.innerWidth > 780 && wasButtonClickedOnMobile) {
+    // wasButtonClickedOnDesktop = false;
+  } else if (
+    window.innerWidth > 869 &&
+    btnProfile.getAttribute("aria-expanded") === "true"
+  ) {
     // Reset to default state for desktop view
     defaultState();
-    wasButtonClickedOnMobile = false;
+    // wasButtonClickedOnMobile = false;
   }
 });
